968b322dbbf188fe6050e122d8b3448b00d1746b
Giordano Cassini <giordano.cassini@gmail.com>
2022-09-07 00:19:19 -0300

    feat: delete user working

--- src/modules/User/UserController.ts
+++ src/modules/User/UserController.ts
@@ -5,31 +5,44 @@ import { validate } from "class-validator";
 import { userRepository } from "../../repositories/userRepository";
 
 export class UserController {
-   static async createUser(req: Request, res: Response) {
-    console.log("entrou no metodo");
-    
+  static async createUser(req: Request, res: Response) {
     const { name, email, password, apartment } = req.body;
-    const user: User = new User();
-    user.name = name;
-    user.password = bcrypt.hashSync(password, 10);
-    user.apartment = apartment;
-    user.email = email;
+    const encryptedPw = bcrypt.hashSync(password, 10);
+    const user: User = userRepository.create({
+      name,
+      email,
+      password: encryptedPw,
+      apartment,
+    });
 
     const errors = await validate(user);
     if (errors.length > 0) {
       return res.status(400).send(errors);
     }
-    
+
     try {
       await userRepository.save(user);
     } catch (error) {
       return res.status(400).send(error);
     }
-
     return res.status(201).json(user);
   }
 
-  /* async deleteUser(req: Request, res: Response) {
+  static async deleteUser(req: Request, res: Response) {
+    const id = req.params.id;
+    let user: User;
+    try {
+      user = await userRepository.findOneOrFail({ where: { id: Number(id) } });
+    } catch (error) {
+      return res.status(404).send("User not found");
+    }
 
-  } */
+    try {
+      userRepository.delete(id);
+    } catch (error) {
+      return res.status(500).send("Server crashed");
+    }
+
+    return res.status(204).send();
+  }
 }
--- src/modules/User/routes.ts
+++ src/modules/User/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {UserController} from "./UserController";
+import { UserController } from "./UserController";
 
 const routes = Router();
 
 routes.post("/user", UserController.createUser);
+routes.delete("/user/:id([0-9]+)", UserController.deleteUser);
 
 export default routes;
