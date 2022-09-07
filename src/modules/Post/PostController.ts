1aa1ca1834c619735e4ca62de06c9355623737c5
Giordano Cassini <giordano.cassini@gmail.com>
2022-09-07 20:11:18 -0300

    refactor: try catch UserController

--- src/modules/Post/PostController.ts
+++ src/modules/Post/PostController.ts
@@ -1,88 +1,84 @@
-import { userRepository } from './../../repositories/userRepository';
+import { userRepository } from "./../../repositories/userRepository";
 import { Response, Request } from "express";
 import { validate } from "class-validator";
 import { postRepository } from "../../repositories/postRepository";
 
 export class PostController {
-  static async createPost(req: Request, res: Response) {
+  /* static async createPost(req: Request, res: Response) {
     const { content } = req.body;
 
-    try {
-      const newPost = postRepository.create({ content })
-
-      await postRepository.save(newPost)
-
-      return res.status(201).json(newPost)
-    } catch (error) {
-      return res.status(500).json({ message: 'Internal Server Error'})
+    const newPost = postRepository.create({ content });
+    const errors = await validate(newPost);
+    if (errors.length > 0) {
+      return res.status(400).send(errors);
     }
 
-  }
+    try {
+      await postRepository.save(newPost);
+      return res.status(201).json(newPost);
+    } catch (error) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  } */
 
   static async createPostbyIdUser(req: Request, res: Response) {
     const { content } = req.body;
     const { idUser } = req.params;
 
-    try { 
-      const user_id = await userRepository.findOneBy({id: Number(idUser)})
+    
 
+    try {
+      const user_id = await userRepository.findOneBy({ id: Number(idUser) });
       if (!user_id) {
-        return res.status(404).json({message: "User does not exist"})
+        return res.status(404).json({ message: "User does not exist" });
       }
-
       const newPost = postRepository.create({
         content,
-        user_id
-       })
-
-       await postRepository.save(newPost)
-
-       return res.status(201).json(newPost)
-      
+        user_id,
+      });
+      await postRepository.save(newPost);
+      return res.status(201).json(newPost);
     } catch (error) {
-      console.log(error)
-      return res.status(500).json({ message: 'Internal Server Error'})
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
     }
   }
 
   static async getPostAll(req: Request, res: Response) {
-
     try {
       const allPost = await postRepository.find({
         relations: {
           user_id: true,
         },
-      })
+      });
 
-      return res.send(allPost)
+      return res.send(allPost);
     } catch (error) {
-      console.log(error)
-      return res.status(500).json({ message: 'Internal Server Error'})
+      console.log(error);
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-
   }
-  
-  static async getPostbyUserId (req: Request, res: Response) {
+
+  static async getPostbyUserId(req: Request, res: Response) {
     const { idUser } = req.params;
-    
-    if(!idUser) {
-      return res.status(404).json({message: "User does not exist"})
+
+    if (!idUser) {
+      return res.status(404).json({ message: "User does not exist" });
     }
 
     try {
       const allPostByIdUser = await userRepository.findOne({
         where: {
-          id: parseInt(idUser)
+          id: parseInt(idUser),
         },
         relations: {
           posts: true,
         },
-      })
+      });
 
-      return res.send(allPostByIdUser)
+      return res.send(allPostByIdUser);
     } catch (error) {
-      return res.status(500).json({ message: 'Internal Server Error'})
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-
   }
 }
