5b248e5557ae00cc1c1ea752185b23b34f651cc2
Giordano Cassini <giordano.cassini@gmail.com>
2022-09-07 22:12:41 -0300

    feat: tratamento de erros PostController refactor: PostController, UserCOntroller

--- src/modules/Post/routes.ts
+++ src/modules/Post/routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 //router.post("/post", [auth], PostController.createPost)
 router.post("/post/user/:idUser([0-9]+)", [auth], PostController.createPostbyIdUser)
-router.get("/post", [auth], PostController.getPostAll)
+router.get("/post", [auth], PostController.getAllPosts)
 router.get("/post/user/:idUser([0-9]+)", [auth], PostController.getPostbyUserId)
 
 export default router;
